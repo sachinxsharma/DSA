8a6cef3c0af44ab8f87e4d838d40254b0400ca58
sachinxsharma <sachinsharma72003@gmail.com>
2023-07-26 18:01:10 +0530

    Initial commit

---
+++ setMismatch.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class setMismatch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 4 };
+        int[] result = findMismatch(arr);
+        System.out.println("Mismatch: " + Arrays.toString(result));
+    }
+
+    static int[] findMismatch(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return new int[] { arr[index], index + 1 };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
